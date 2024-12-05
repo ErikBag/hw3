7bc6792ca0387896119ec7a48cca0fd5f339e37d
Erik <erik.ata.1128@mail.ru>
2024-12-05 05:16:09 +0300

    Новый тест TreiberStackTest6 (checkObstructionFreedom)

--- src/test/kotlin/org/example/test.kt
+++ src/test/kotlin/org/example/test.kt
@@ -3,6 +3,7 @@ package org.example
 import org.jetbrains.kotlinx.lincheck.annotations.Operation
 import org.jetbrains.kotlinx.lincheck.check
 import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingOptions
+import org.jetbrains.kotlinx.lincheck.annotations.*
 import org.jetbrains.kotlinx.lincheck.strategy.stress.StressOptions
 import org.junit.jupiter.api.Test
 import java.util.*
@@ -112,5 +113,26 @@ class TreiberStackTest5 {
             .iterations(20)
             .invocationsPerIteration(1000)
             .check(TreiberStackTest5::class)
+    }
+}
+
+class TreiberStackTest6 {
+    private val stack = TreiberStack<Int>()
+
+    @Operation
+    fun add(x: Int) {
+        stack.add(x)
+    }
+
+    @Operation
+    fun remove(): Int? {
+        return stack.remove()
+    }
+
+    @Test
+    fun test() {
+        ModelCheckingOptions()
+            .checkObstructionFreedom()
+            .check(TreiberStackTest6::class)
     }
 }
